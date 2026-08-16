05419afd2cf2ef2b40dac1b8fc11bafb3b397d9a
Reguluspt <130158621+Reguluspt@users.noreply.github.com>
2026-08-16 20:48:59 +0700

    E0-PR-002: scope Astryx vendor global selectors

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {
+  isReAstryxVendorCss,
+  scopeReAstryxVendorCss,
+} from './scripts/scope-re-astryx-css.mjs'
+
+function reAstryxCssContainmentPlugin() {
+  return {
+    name: 'cenvalue-re-astryx-css-containment',
+    enforce: 'pre',
+    transform(code, id) {
+      if (!isReAstryxVendorCss(id)) return null
+      return {
+        code: scopeReAstryxVendorCss(code, id),
+        map: null,
+      }
+    },
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reAstryxCssContainmentPlugin(), react()],
   server: {
     port: 5173,
     proxy: {
