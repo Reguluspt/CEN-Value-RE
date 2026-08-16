b4f6bf67084adc8ff2e451fad378c95897df84a3
Reguluspt <130158621+Reguluspt@users.noreply.github.com>
2026-08-16 20:56:34 +0700

    E0-PR-002: use generated scoped CSS instead of Vite vendor transform

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,27 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {
-  isReAstryxVendorCss,
-  scopeReAstryxVendorCss,
-} from './scripts/scope-re-astryx-css.mjs'
-
-function reAstryxCssContainmentPlugin() {
-  return {
-    name: 'cenvalue-re-astryx-css-containment',
-    enforce: 'pre',
-    transform(code, id) {
-      if (!isReAstryxVendorCss(id)) return null
-      return {
-        code: scopeReAstryxVendorCss(code, id),
-        map: null,
-      }
-    },
-  }
-}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reAstryxCssContainmentPlugin(), react()],
+  plugins: [react()],
   server: {
     port: 5173,
     proxy: {
