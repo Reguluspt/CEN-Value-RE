debe1a28b161ee74b0826f044741db4e9451980d
Reguluspt <130158621+Reguluspt@users.noreply.github.com>
2026-08-17 15:07:26 +0700

    E1-PR-002: export adjustment domain contracts

--- src/re/domain/adjustment/__init__.py
+++ src/re/domain/adjustment/__init__.py
@@ -1,1 +1,29 @@
 """CenValue RE adjustment domain package."""
+
+from .calculation import (
+    AdjustmentFactorDefinition,
+    AdjustmentRunSnapshot,
+    AdjustmentStep,
+    ComparableLandUnitPriceSnapshot,
+    IncompleteAdjustmentDecisionError,
+    N08_ADJUSTMENT_FACTORS,
+    N08_FACTOR_KEYS,
+    SelectedAdjustmentDecision,
+    calculate_adjustment_run,
+    derive_comparable_land_unit_price,
+    normalize_market_price,
+)
+
+__all__ = [
+    "AdjustmentFactorDefinition",
+    "AdjustmentRunSnapshot",
+    "AdjustmentStep",
+    "ComparableLandUnitPriceSnapshot",
+    "IncompleteAdjustmentDecisionError",
+    "N08_ADJUSTMENT_FACTORS",
+    "N08_FACTOR_KEYS",
+    "SelectedAdjustmentDecision",
+    "calculate_adjustment_run",
+    "derive_comparable_land_unit_price",
+    "normalize_market_price",
+]
